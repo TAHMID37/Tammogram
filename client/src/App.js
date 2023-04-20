7b766a3ca85be167c4e4cc9c00d60f5a1388b91a
Tahmid Rahman <190041237tr@gmail.com>
2023-04-20 21:11:34 +0600

    login register Auth validation completed

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ function App() {
 
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
     <div className="app">
@@ -25,9 +26,15 @@ function App() {
 
         
         <Route path="/" element={<LoginPage/>}/>
-        <Route path="/home" element={<HomePage/>}/>
-        <Route path="/profile/:userId" element={<ProfilePage/>}/>
-
+        <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+        />
+        
+        <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+        />
         
          
        </Routes>
